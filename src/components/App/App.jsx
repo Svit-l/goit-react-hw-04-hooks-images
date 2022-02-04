16f83f5c79c180aa487b906633c023054a5cffd1
Svit-l <lov.svit@gmail.com>
2022-02-04 02:16:49 +0200

    refactor: App and modal to hooks wuthout DidMount

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
-import { Component } from 'react';
+import { useState, useEffect, useMemo } from 'react';
+
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { animateScroll as scroll } from 'react-scroll';
 
 import s from './App.module.css';
+// import fetchImages from '../../services';
 import ButtonLoadMore from '../ButtonLoadMore';
 import ImageGallery from '../ImageGallery';
 import Loader from '../Loader';
@@ -13,46 +15,43 @@ import Searchbar from '../Searchbar';
 
 const API_KEY = '24539365-a9ec93e41963d169f0a4900c0';
 
-class App extends Component {
-  state = {
-    searchString: '',
-    pictures: [],
-    page: 1,
-    itemsPerPage: 12,
-    loading: false,
-    showModal: false,
-    selectedFhotoUrl: '',
-    error: null,
-  };
+function App() {
+  const [searchString, setSearchString] = useState('');
+  const [pictures, setPictures] = useState([]);
+  const [page, setPpage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(12);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedFhotoUrl, setSelectedFhotoUrl] = useState('');
+  const [error, setError] = useState(null);
 
-  componentDidUpdate(_, prevState) {
-    if (prevState.searchString !== this.state.searchString) {
-      this.setState({
-        page: 1,
-        itemsPerPage: 12,
-        pictures: [],
-        loading: true,
-        error: null,
-      });
-      this.findImages();
-      window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'auto',
-      });
-    }
-    if (
-      prevState.itemsPerPage !== this.state.itemsPerPage &&
-      prevState.searchString === this.state.searchString
-    ) {
-      this.setState({ loading: true, error: null });
-      this.findImages();
-      scroll.scrollMore(610);
-    }
-  }
+  // componentDidUpdate(_, prevState) {
+  //   if (prevState.searchString !== this.state.searchString) {
+  //     this.setState({
+  //       page: 1,
+  //       itemsPerPage: 12,
+  //       pictures: [],
+  //       loading: true,
+  //       error: null,
+  //     });
+  //     this.findImages();
+  //     window.scrollTo({
+  //       top: 0,
+  //       left: 0,
+  //       behavior: 'auto',
+  //     });
+  //   }
+  //   if (
+  //     prevState.itemsPerPage !== this.state.itemsPerPage &&
+  //     prevState.searchString === this.state.searchString
+  //   ) {
+  //     this.setState({ loading: true, error: null });
+  //     this.findImages();
+  //     scroll.scrollMore(610);
+  //   }
+  // }
 
-  findImages = () => {
-    const { searchString, page, itemsPerPage } = this.state;
+  const findImages = () => {
     fetch(
       `https://pixabay.com/api/?q=${searchString}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${itemsPerPage}`
     )
@@ -71,7 +70,7 @@ class App extends Component {
             theme: 'colored',
           });
         } else
-          this.setState({
+          setPictures({
             pictures: res.hits.map(item => ({
               id: item.id,
               webformatURL: item.webformatURL,
@@ -81,92 +80,79 @@ class App extends Component {
           });
         return;
       })
-      .catch(error => this.setState({ error }))
-      .finally(() => this.setState({ loading: false }));
+      .catch(error => setError(error))
+      .finally(() => setLoading(false));
   };
 
-  handleSearchSubmit = searchString => {
-    this.setState({ searchString: searchString });
-
-    this.setState({ page: 1, itemsPerPage: 12 });
+  const handleSearchSubmit = searchString => {
+    setSearchString(searchString);
+    setPpage(1);
+    setItemsPerPage(12);
     // console.log('fotos loading');
     // console.log(searchString);
   };
 
-  changePage = () => {
-    this.setState(prevState => ({
-      // page: prevState.page + 1,
-      itemsPerPage: prevState.itemsPerPage + 12,
-    }));
-  };
-
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
-  };
-
-  setModalImg = largeImgUrl => {
-    this.setState(() => ({
-      selectedFhotoUrl: largeImgUrl,
-    }));
-    this.toggleModal();
-  };
-
-  render() {
-    const { toggleModal, handleSearchSubmit, changePage, setModalImg } = this;
-    const {
-      pictures,
-      loading,
-      showModal,
-      error,
-      selectedFhotoUrl,
-      itemsPerPage,
-    } = this.state;
-
-    return (
-      <div className={s.app}>
-        {showModal && (
-          <>
-            <Modal onClose={toggleModal}>
-              <img src={selectedFhotoUrl} alt="Large fhoto" />
-            </Modal>
-          </>
-        )}
-
-        <Searchbar propsSubmit={handleSearchSubmit} />
-
-        {loading && <Loader />}
-
-        {error && <h1>{error.message}</h1>}
-
-        {pictures.length > 0 ? (
-          <ImageGallery
-            items={pictures}
-            onClick={largeImageURL => {
-              setModalImg(largeImageURL);
-            }}
-          />
-        ) : (
-          <div className={s.string}>
-            Enter keyword for search images and fhotos!
-          </div>
-        )}
-
-        {/* {console.log(pictures.length)} */}
-
-        {pictures.length > itemsPerPage - 1 && !loading && (
-          <ButtonLoadMore onClick={() => changePage()} />
-        )}
-
-        <ToastContainer
-          position="top-center"
-          autoClose={2500}
-          fontSize="12pt"
-          hideProgressBar
-          style={{ width: 'auto' }}
-        />
-      </div>
+  const changePage = () => {
+    setItemsPerPage(
+      prevState =>
+        // page: prevState.page + 1,
+        prevState + itemsPerPage
     );
-  }
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const setModalImg = largeImgUrl => {
+    setSelectedFhotoUrl(largeImgUrl);
+    toggleModal();
+  };
+
+  return (
+    <div className={s.app}>
+      {showModal && (
+        <>
+          <Modal onClose={toggleModal}>
+            <img src={selectedFhotoUrl} alt="Large fhoto" />
+          </Modal>
+        </>
+      )}
+
+      <Searchbar propsSubmit={handleSearchSubmit} />
+
+      {loading && <Loader />}
+
+      {error && <h1>{error.message}</h1>}
+
+      {pictures.length > 0 ? (
+        <ImageGallery
+          items={pictures}
+          onClick={largeImageURL => {
+            setModalImg(largeImageURL);
+          }}
+        />
+      ) : (
+        <div className={s.string}>
+          Enter keyword for search images and fhotos!
+        </div>
+      )}
+
+      {/* {console.log(pictures.length)} */}
+
+      {pictures.length > itemsPerPage - 1 && !loading && (
+        <ButtonLoadMore onClick={() => changePage()} />
+      )}
+
+      <ToastContainer
+        position="top-center"
+        autoClose={2500}
+        fontSize="12pt"
+        hideProgressBar
+        style={{ width: 'auto' }}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { createPortal } from 'react-dom';
 
 import PropTypes from 'prop-types';
@@ -6,43 +6,43 @@ import s from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  static propTypes = {
-    onClose: PropTypes.func.isRequired,
-  };
+function Modal(onClose) {
+  // const [filtered, setFiltered] = useState('');
 
-  componentDidMount() {
-    // console.log('Modal componentDidMount');
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+  // componentDidMount() {
+  //   // console.log('Modal componentDidMount');
+  //   window.addEventListener('keydown', this.handleKeyDown);
+  // }
 
-  componentWillUnmount() {
-    // console.log('Modal componentWillUnmount');
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+  // componentWillUnmount() {
+  //   // console.log('Modal componentWillUnmount');
+  //   window.removeEventListener('keydown', this.handleKeyDown);
+  // }
 
-  handleKeyDown = e => {
+  const handleKeyDown = e => {
     if (e.code === 'Escape') {
       // console.log('Press Esc');
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleBackdropClick = e => {
+  const handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
       // console.log('click backdrope');
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <div className={s.overlay} onClick={this.handleBackdropClick}>
-        <div className={s.modal}>{this.props.children}</div>
-      </div>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <div className={s.overlay} onClick={handleBackdropClick}>
+      <div className={s.modal}>{this.props.children}</div>
+    </div>,
+    modalRoot
+  );
 }
 
 export default Modal;
+
+// propTypes = {
+//     onClose: PropTypes.func.isRequired,
+//   };
